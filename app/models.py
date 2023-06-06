a6cec22fed4dd4ccf20a09d5f0272d6e983290ad
Pankaj Jaiswal <pankaj@Pankajs-MacBook-Air.local>
2023-06-06 13:47:07 +0530

    added task,tag model & setup admin site

--- app/models.py
+++ app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here. 
+class Task(models.Model):
+    STATUS_CHOICES = (
+        ('OPEN', 'Open'),
+        ('WORKING', 'Working'),
+        ('DONE', 'Done'),
+        ('OVERDUE', 'Overdue'),
+    )
+
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000)
+    tags = models.ManyToManyField('Tag', blank=True)
+    created_at = models.DateTimeField(auto_now=True)
+    due_date = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='OPEN')
+    
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
