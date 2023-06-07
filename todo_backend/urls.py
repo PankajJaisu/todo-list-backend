c7381481a24d54d83b4dcbecdf549944b883531f
Pankaj Jaiswal <pankaj@Pankajs-MacBook-Air.local>
2023-06-07 12:37:18 +0530

    updated urls.py

--- todo_backend/urls.py
+++ todo_backend/urls.py
@@ -1,5 +1,6 @@
 
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path,include
 urlpatterns = [
@@ -8,3 +9,5 @@ urlpatterns = [
     # path('api/todo/<int:id>/',views.TaskView.as_view()),
 
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
